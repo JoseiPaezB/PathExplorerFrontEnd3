984c79f3c846cd2a5e03a35aa4d3c7fffb285305
Gustavo Coutiño <gcoutino04@gmail.com>
2025-06-09 16:36:48 -0600

    Changed hook error

--- front/hooks/useHasPendingRequest.ts
+++ front/hooks/useHasPendingRequest.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api";
 
 interface PendingRequestResponse {
   hasPendingRequest: boolean;
